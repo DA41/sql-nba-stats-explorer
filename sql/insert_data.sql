1fd5e26a194a34fa6d2e83e1bfb90be7aa7e2ea3
David Adewole <davidadewole16@gmail.com>
2026-03-06 13:07:29 -0700

    Add insert_data.sql

---
+++ sql/insert_data.sql
@@ -0,0 +1,9 @@
+-- Inserts sample NBA player stats into the player_stats table (Stats are taken from the 'Real' app and the 'NBA' app, and are as of March 5, 2026)
+INSERT INTO player_stats (player_name, team, position, games_played, ppg, rpg, apg, fg_pct)
+VALUES
+("Giannis Antetokounmpo", "Milwaukee Bucks", "PF", 32, 27.6, 9.9, 5.4, 63.7),
+("Luka Doncic", "Los Angeles Lakers", "PG", 50, 32.3, 7.8, 8.6, 47.3),
+("Lebron James", "Los Angeles Lakers", "SF", 44, 21.4, 5.6, 7.0, 50.4),
+("Stephen Curry", "Golden State Warriors", "PG", 38, 27.3, 3.5, 4.8, 46.8),
+("Nikola Jokic", "Denver Nuggets", "C", 47, 28.6, 12.6, 10.4, 57.2),
+("Shai Gilgeous-Alexander", "Oklahoma City Thunder", "SG", 52, 31.7, 4.4, 6.5. 55.1)
